866bdbdf90823d2b0b3c56ccfae3a55173e64c92
Alfonso Patron <c9alfonsopatron@hotmail.com>
2023-12-30 22:40:24 -0700

    Task List Works. - Add tasks works - Select task with text and radius button works
    -To fix
    when select the radius button, the text dont striked

--- app/src/main/java/com/patron/todoapp/TasksViewHolder.kt
+++ app/src/main/java/com/patron/todoapp/TasksViewHolder.kt
@@ -1,10 +1,12 @@
 package com.patron.todoapp
 
+import android.content.res.ColorStateList
 import android.graphics.Paint
 import android.util.Log
 import android.view.View
 import android.widget.CheckBox
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class TasksViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -21,7 +23,20 @@ class TasksViewHolder(view: View) : RecyclerView.ViewHolder(view) {
             tvTask.paintFlags = tvTask.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
 
         }
+
         tvTask.text = task.name
+        checkboxTask.isChecked = task.isSelected
+
+        val color = when(task.category){
+            TaskCategory.business -> R.color.todo_business_category
+            TaskCategory.others -> R.color.todo_other_category
+            TaskCategory.personal -> R.color.todo_personal_category
+        }
+
+        checkboxTask.buttonTintList = ColorStateList.valueOf(
+            ContextCompat.getColor(checkboxTask.context, color)
+        )
+
 
     }
 
